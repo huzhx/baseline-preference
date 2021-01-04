4f5ee5d5be016050a53827493c0fe16504ea3cc5
Zhaoxian Hu <huzhx@uci.edu>
2021-01-04 00:55:42 -0800

    implement the Button component

--- src/SetUpPreference.js
+++ src/SetUpPreference.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 import styles from './SetUpPreference.module.css';
 import Text from './Text';
+import Button from './Button';
 
 const SetUpPreference = () => {
   const greeting = 'Hi! Welcome to iAGREE!';
   const introduction =
     'Since you are a first time user, before we get it started, please set up your data sharing preferences, you can always edit your preferences in the future if needed!';
+  const buttonLabel = 'next';
   return (
     <div className={styles.setup_preference__container}>
-      <Text value={greeting} />
-      <Text value={introduction} />
+      <div className={styles.setup_preference__content_container}>
+        <Text value={greeting} />
+        <Text value={introduction} />
+      </div>
+      <div className={styles.setup_preference__button_container}>
+        <Button label={buttonLabel} />
+      </div>
     </div>
   );
 };
