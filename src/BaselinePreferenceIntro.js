944ca3615e35758be8baf3205a10ba33edbbd432
Zhaoxian Hu <huzhx@uci.edu>
2021-01-06 11:15:42 -0800

    rename SetUpPreference component -> BaselinePreferenceIntro component

--- src/BaselinePreferenceIntro.js
+++ src/BaselinePreferenceIntro.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
-import styles from './SetUpPreference.module.css';
+import styles from './BaselinePreferenceIntro.module.css';
 import Text from './Text';
 import Button from './Button';
 
-const SetUpPreference = () => {
+const BaselinePreferenceIntro = () => {
   const greeting = 'Hi! Welcome to iAGREE!';
   const introduction =
     'Since you are a first time user, before we get it started, please set up your data sharing preferences. You can always edit your preferences in the future if needed!';
   const buttonLabel = 'next';
   return (
-    <div className={styles.setup_preference__container}>
-      <div className={styles.setup_preference__content_container}>
+    <div className={styles.baseline_preference_intro__container}>
+      <div className={styles.baseline_preference_intro__content_container}>
         <Text value={greeting} />
         <Text value={introduction} />
       </div>
-      <div className={styles.setup_preference__button_container}>
+      <div className={styles.baseline_preference_intro__button_container}>
         <Button label={buttonLabel} />
       </div>
     </div>
   );
 };
 
-export default SetUpPreference;
+export default BaselinePreferenceIntro;
