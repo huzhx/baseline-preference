a62606127e032b5b4f7ed0b596a88292d6ea26c3
Zhaoxian Hu <huzhx@uci.edu>
2021-01-11 00:11:32 -0800

    update BaselinePreferenceIntro component to handle the path direction

--- src/BaselinePreferenceIntro.js
+++ src/BaselinePreferenceIntro.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import styles from './BaselinePreferenceIntro.module.css';
 import Text from './Text';
 import Button from './Button';
 
-const BaselinePreferenceIntro = () => {
+const BaselinePreferenceIntro = ({ cb }) => {
   const greeting = 'Hi! Welcome to iAGREE!';
   const introduction =
     'Since you are a first time user, before we get it started, please set up your data sharing preferences. You can always edit your preferences in the future if needed!';
   const buttonLabel = 'next';
+
+  const nextPath = cb('intro');
+  const history = useHistory();
+  const handleClick = () => {
+    history.push(nextPath);
+  };
+
   return (
     <div className={styles.baseline_preference_intro__container}>
       <div className={styles.baseline_preference_intro__content_container}>
@@ -16,7 +24,7 @@ const BaselinePreferenceIntro = () => {
         <Text value={introduction} />
       </div>
       <div className={styles.baseline_preference_intro__button_container}>
-        <Button label={buttonLabel} />
+        <Button label={buttonLabel} handleClick={handleClick} />
       </div>
     </div>
   );
