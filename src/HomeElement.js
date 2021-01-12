3c4f45069db8d6df6cf228b8b7a1582578fd2eb0
Zhaoxian Hu <huzhx@uci.edu>
2021-01-11 23:56:51 -0800

    add HomeElement component

---
+++ src/HomeElement.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './HomeElement.module.css';
+
+const HomeElement = ({ label, unreadNum = 0 }) => {
+  return (
+    <div className={styles.home_element}>
+      {unreadNum > 0 && (
+        <div>
+          <div className={styles.home_element__badge}>{unreadNum}</div>
+        </div>
+      )}
+      <div className={styles.home_element__label}>{label}</div>
+    </div>
+  );
+};
+
+export default HomeElement;
