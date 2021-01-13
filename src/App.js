333dbc8fd4a7de09adbcf1e5d4b7adbdff83ac18
Zhaoxian Hu <huzhx@uci.edu>
2021-01-12 22:07:07 -0800

    implement the DataRequests component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import BaselinePreferenceHandler from './BaselinePreferenceHandler';
 import Home from './Home';
+import DataRequests from './DataRequests';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/baseline-preference">
-          <BaselinePreferenceHandler />;
+          <BaselinePreferenceHandler />
+        </Route>
+        <Route path="/data-requests">
+          <DataRequests />
         </Route>
         <Route path="/">
           <Home />
