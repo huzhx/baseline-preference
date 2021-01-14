f83cf61e9c28647e911a9a81e75ba244bbb632dd
Zhaoxian Hu <huzhx@uci.edu>
2021-01-13 23:33:42 -0800

    update Study component

--- src/Study.js
+++ src/Study.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styles from './Study.module.css';
 
-const Study = ({ institution, title, status }) => {
+const Study = ({ institution, title, status, purpose, researchers, dateRequested }) => {
   return (
     <div className={styles.study}>
       <p className={styles.study__content}>
@@ -17,6 +17,24 @@ const Study = ({ institution, title, status }) => {
         <p className={styles.study__content}>
           <span className={styles['study__content--highlighted']}>Status:</span>
           <span>{status}</span>
+        </p>
+      )}
+      {purpose && (
+        <p className={styles.study__content}>
+          <span className={styles['study__content--highlighted']}>Purpose:</span>
+          <span>{purpose}</span>
+        </p>
+      )}
+      {researchers && (
+        <p className={styles.study__content}>
+          <span className={styles['study__content--highlighted']}>Researchers:</span>
+          <span>{researchers}</span>
+        </p>
+      )}
+      {dateRequested && (
+        <p className={styles.study__content}>
+          <span className={styles['study__content--highlighted']}>Date Requested:</span>
+          <span>{dateRequested}</span>
         </p>
       )}
     </div>
