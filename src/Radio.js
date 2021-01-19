3bc1a88ecd9b2b9204d0f06faa68acc04175cbce
Zhaoxian Hu <huzhx@uci.edu>
2021-01-18 22:42:09 -0800

    correct error in Radio component

--- src/Radio.js
+++ src/Radio.js
@@ -14,9 +14,9 @@ const Radio = ({ option, checked, handleOnChange }) => {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className={styles.icon}
         >
           <polyline points="20 6 9 17 4 12" />
