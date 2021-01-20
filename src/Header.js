a235274c0f67ff0f777f7b1a9e285ee651969341
Zhaoxian Hu <huzhx@uci.edu>
2021-01-19 23:19:43 -0800

    update Header and take highlight content

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import styles from './Header.module.css';
 
-const Header = ({ title }) => {
-  return <div className={styles.header}>{title}</div>;
+const Header = ({ title, highlights }) => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.header__main}>
+        {title}
+        {highlights && <span className={styles['header--highlighted']}> {highlights} Information</span>}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
