7be7684b69cbc18f91b3cd46f83ae3efb503ecd2
Zhaoxian Hu <huzhx@uci.edu>
2021-01-19 18:11:44 -0800

    implement the IconButton component

--- src/DataRequests.js
+++ src/DataRequests.js
@@ -5,6 +5,7 @@ import Study from './Study';
 import Header from './Header';
 import Footer from './Footer';
 import { useScrollPosition } from './UseScrollPosition';
+import IconButton from './IconButton';
 
 const DataRequests = () => {
   const [sticky, setSticky] = useState(false);
@@ -37,8 +38,40 @@ const DataRequests = () => {
         <div className={styles.data_requests__content}>{requests}</div>
       </div>
       <Footer alignContentEvenly={true} sticky={sticky}>
-        <div>Home</div>
-        <div>My profile</div>
+        <IconButton label="Home">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-home"
+          >
+            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+          </svg>
+        </IconButton>
+        <IconButton label="My profile">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-user"
+          >
+            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+            <circle cx="12" cy="7" r="4"></circle>
+          </svg>
+        </IconButton>
       </Footer>
     </div>
   );
