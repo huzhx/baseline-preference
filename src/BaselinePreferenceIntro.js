e67192273fbe37f648d52248507736753b7b8576
Zhaoxian Hu <huzhx@uci.edu>
2021-01-19 19:29:59 -0800

    update BaselinePreferenceIntro component style

--- src/BaselinePreferenceIntro.js
+++ src/BaselinePreferenceIntro.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import styles from './BaselinePreferenceIntro.module.css';
 import Text from './Text';
 import Button from './Button';
+import Header from './Header';
+import Footer from './Footer';
+import { useScrollPosition } from './UseScrollPosition';
 
 const BaselinePreferenceIntro = ({ cb }) => {
+  const [sticky, setSticky] = useState(false);
+  useScrollPosition(
+    ({ prevPos, currPos }) => {
+      const isShow = currPos.y > prevPos.y;
+      if (isShow !== sticky) setSticky(isShow);
+    },
+    [sticky]
+  );
+
   const greeting = 'Hi! Welcome to iAGREE!';
   const introduction =
     'Since you are a first time user, before we get it started, please set up your data sharing preferences. You can always edit your preferences in the future if needed!';
@@ -18,14 +30,17 @@ const BaselinePreferenceIntro = ({ cb }) => {
   };
 
   return (
-    <div className={styles.baseline_preference_intro__container}>
-      <div className={styles.baseline_preference_intro__content_container}>
-        <Text value={greeting} />
-        <Text value={introduction} />
+    <div className={styles.baseline_preference_intro}>
+      <Header title="iAGREE" />
+      <div className={styles.baseline_preference_intro__body}>
+        <div className={styles.baseline_preference_intro__content}>
+          <Text value={greeting} />
+          <Text value={introduction} />
+        </div>
       </div>
-      <div className={styles.baseline_preference_intro__button_container}>
+      <Footer alignContentEvenly={false} sticky={sticky}>
         <Button label={buttonLabel} handleClick={handleClick} />
-      </div>
+      </Footer>
     </div>
   );
 };
