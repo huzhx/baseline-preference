f7b11b18a1ff33e4e83fcd73fd8d353e5cdcdd3d
Zhaoxian Hu <huzhx@uci.edu>
2021-01-20 22:58:23 -0800

    update Checkbox component

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import styles from './Checkbox.module.css';
 
-const Checkbox = ({ checked, handleOnChange }) => {
+const Checkbox = ({ checked, onChange }) => {
   return (
     <label className={styles.checkbox}>
-      <input type="checkbox" checked={checked} onChange={handleOnChange} />
+      <input type="checkbox" checked={checked} onChange={onChange} />
       <span className={styles.overlay}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
