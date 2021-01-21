0bd75b426bf5644a39a3bec698b94e7424818f6c
Zhaoxian Hu <huzhx@uci.edu>
2021-01-21 13:51:58 -0800

    add PortalPicker component

---
+++ src/PortalPicker.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import styles from './PortalPicker.module.css';
+import AuthButton from './AuthButton';
+
+const PortalPicker = () => {
+  return (
+    <div className={styles.portal_picker}>
+      <div className={styles.portal_picker__header}>
+        <div className={[styles.portal_picker__header__secondary, styles.secondary_color].join(' ')}>Welcome to</div>
+        <div className={styles.portal_picker__header__main}>iAGREE</div>
+      </div>
+      <div className={styles.portal_picker__body}>
+        <div className={styles.portal_picker__content}>
+          <div className={styles.portal_picker__content__label}>Please chose your portal</div>
+          <AuthButton stroke label="Provider Portal" />
+          <AuthButton label="Patient Portal" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PortalPicker;
