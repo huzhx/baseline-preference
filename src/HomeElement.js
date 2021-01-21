11570549f7e04b0d88df19922c502603c66080b0
Zhaoxian Hu <huzhx@uci.edu>
2021-01-20 16:42:29 -0800

    adjust the HomeElement layout to use grid instead of flex

--- src/HomeElement.js
+++ src/HomeElement.js
@@ -5,10 +5,12 @@ import styles from './HomeElement.module.css';
 const HomeElement = ({ label, unreadNum = 0 }) => {
   return (
     <div className={[styles.home_element, styles.ripple].join(' ')}>
-      {unreadNum > 0 && (
+      {unreadNum > 0 ? (
         <div>
           <div className={styles.home_element__badge}>{unreadNum}</div>
         </div>
+      ) : (
+        <div></div>
       )}
       <div className={styles.home_element__label}>{label}</div>
     </div>
