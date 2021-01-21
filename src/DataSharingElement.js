1c8fcb13c4b40b84fb65bc585c41e4708a5ddc45
Zhaoxian Hu <huzhx@uci.edu>
2021-01-20 23:11:12 -0800

    update DataSharingElement

--- src/DataSharingElement.js
+++ src/DataSharingElement.js
@@ -3,23 +3,23 @@ import React from 'react';
 import styles from './DataSharingElement.module.css';
 import Checkbox from './Checkbox';
 
-const DataSharingElement = ({ dataElement, isRequired, isConsented, isUsed }) => {
+const DataSharingElement = ({ dataElement, required, used, checked, onChange }) => {
   return (
     <div
       className={
-        typeof isUsed === 'undefined'
+        typeof used === 'undefined'
           ? styles.data_sharing_element
           : [styles.data_sharing_element, styles['data_sharing_element--extra-column']].join(' ')
       }
     >
       <div className={styles.data_sharing_element__data_element}>
         {dataElement}
-        {isRequired && <span className={styles['data_sharing_element__data_element--required']}>*</span>}
+        {required && <span className={styles['data_sharing_element__data_element--required']}>*</span>}
       </div>
       <div className={styles.data_sharing_element__checker}>
-        <Checkbox checked={isConsented && isConsented.has(dataElement)} />
+        <Checkbox checked={checked} onChange={onChange} />
       </div>
-      {isUsed && <div className={styles.data_sharing_element__label}>{isUsed.has(dataElement) ? 'True' : 'False'}</div>}
+      {used && <div className={styles.data_sharing_element__label}>{used ? 'True' : 'False'}</div>}
     </div>
   );
 };
