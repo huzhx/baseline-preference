95ac7a640f277b85c0254b31882e02945fb88ba5
Zhaoxian Hu <huzhx@uci.edu>
2021-01-22 01:43:04 -0800

    add routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import BaselinePreferenceHandler from './BaselinePreferenceHandler';
 import Home from './Home';
 import DataRequests from './DataRequests';
+import StudyInfo from './StudyInfo';
+import StudyDataSharing from './StudyDataSharing';
+import StudyDeclineSurvey from './StudyDeclineSurvey';
+import ConsentHistory from './ConsentHistory';
+import StudyDataSharingReview from './StudyDataSharingReview';
+import PortalPicker from './PortalPicker';
+import Portal from './Portal';
+import SignIn from './SignIn';
+import SignInVeriOpt from './SignInVeriOpt';
+import SignInVeriEnterCode from './SignInVeriEnterCode';
 
 function App() {
   return (
@@ -13,6 +23,36 @@ function App() {
         </Route>
         <Route path="/data-requests">
           <DataRequests />
+        </Route>
+        <Route path="/study-info">
+          <StudyInfo />
+        </Route>
+        <Route path="/consent-history">
+          <ConsentHistory />
+        </Route>
+        <Route path="/data-sharing-setting">
+          <StudyDataSharing />
+        </Route>
+        <Route path="/data-sharing-review">
+          <StudyDataSharingReview />
+        </Route>
+        <Route path="/decline-survey">
+          <StudyDeclineSurvey />
+        </Route>
+        <Route path="/portal-pick">
+          <PortalPicker />
+        </Route>
+        <Route path="/portal/patient">
+          <Portal portalType="patient" />
+        </Route>
+        <Route path="/sign-in/verification-options">
+          <SignInVeriOpt />
+        </Route>
+        <Route path="/sign-in/enter-code">
+          <SignInVeriEnterCode />
+        </Route>
+        <Route path="/sign-in">
+          <SignIn />
         </Route>
         <Route path="/">
           <Home />
