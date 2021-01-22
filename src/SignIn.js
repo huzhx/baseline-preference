7d30b046678e2789e0fddee8eca62f6eda966994
Zhaoxian Hu <huzhx@uci.edu>
2021-01-21 22:47:46 -0800

    add SignIn component

---
+++ src/SignIn.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import styles from './SignIn.module.css';
+import Header from './Header.js';
+import IconCircleButton from './IconCircleButton';
+
+const SignIn = () => {
+  return (
+    <div className={styles.sign_in}>
+      <Header title="Sign in with Social Network" />
+      <div className={styles.sign_in__body}>
+        <div className={styles.sign_in__content}>
+          <div className={styles.sign_in__content__button_container}>
+            <IconCircleButton>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+              </svg>
+            </IconCircleButton>
+            <IconCircleButton>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M11.99 13.9v-3.72h9.36c.14.63.25 1.22.25 2.05c0 5.71-3.83 9.77-9.6 9.77c-5.52 0-10-4.48-10-10S6.48 2 12 2c2.7 0 4.96.99 6.69 2.61l-2.84 2.76c-.72-.68-1.98-1.48-3.85-1.48c-3.31 0-6.01 2.75-6.01 6.12s2.7 6.12 6.01 6.12c3.83 0 5.24-2.65 5.5-4.22h-5.51v-.01z" />
+              </svg>
+            </IconCircleButton>
+            <IconCircleButton>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+              </svg>
+            </IconCircleButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
