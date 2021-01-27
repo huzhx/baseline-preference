6aa0cec4beefe7f45aa6f376f0fe619c77aa2ac4
Zhaoxian Hu <huzhx@uci.edu>
2021-01-26 21:34:37 -0800

    implement NavBar component

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ import Header from './Header';
 import Footer from './Footer';
 import { useScrollPosition } from './UseScrollPosition';
 import IconButton from './IconButton';
+import NavBar from './NavBar';
 
 const Home = () => {
   const [sticky, setSticky] = useState(false);
@@ -19,6 +20,24 @@ const Home = () => {
 
   return (
     <div className={styles.home}>
+      <div className={styles.home__nav_bar}>
+        <NavBar>
+          <IconButton label="My profile" flat>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+          </IconButton>
+        </NavBar>
+      </div>
       <Header title="iAGREE" />
       <div className={styles.home__body}>
         <div className={styles.home__content}>
