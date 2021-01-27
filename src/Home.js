87928af0bdcdff4086258d4476243028fd905f88
Zhaoxian Hu <huzhx@uci.edu>
2021-01-26 22:24:06 -0800

    add Home button

--- src/Home.js
+++ src/Home.js
@@ -22,6 +22,20 @@ const Home = () => {
     <div className={styles.home}>
       <div className={styles.home__nav_bar}>
         <NavBar>
+          <IconButton label="iAgree" flat>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            </svg>
+          </IconButton>
           <IconButton label="My profile" flat>
             <svg
               xmlns="http://www.w3.org/2000/svg"
