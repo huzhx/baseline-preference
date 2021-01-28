2d7666d28ac392e36eea1cb5a1fc5b21660abd65
Zhaoxian Hu <huzhx@uci.edu>
2021-01-28 00:00:00 -0800

    stop hard coding the size of the icon

--- src/ConsentHistory.js
+++ src/ConsentHistory.js
@@ -71,8 +71,6 @@ const ConsentHistory = () => {
         <IconButton label="Home">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
@@ -87,8 +85,6 @@ const ConsentHistory = () => {
         <IconButton label="My profile">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
