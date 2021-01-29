dbd7026302640cda7e8e53e8e5fda3debb795528
Zhaoxian Hu <huzhx@uci.edu>
2021-01-28 23:26:08 -0800

    update StudyDeclineSurvey style to be responsive

--- src/StudyDeclineSurvey.js
+++ src/StudyDeclineSurvey.js
@@ -7,6 +7,8 @@ import Footer from './Footer';
 import Button from './Button';
 import { useScrollPosition } from './UseScrollPosition';
 import Textarea from './Textarea';
+import IconButton from './IconButton';
+import NavBar from './NavBar';
 
 const StudyDeclineSurvey = () => {
   const [sticky, setSticky] = useState(false);
@@ -56,25 +58,69 @@ const StudyDeclineSurvey = () => {
 
   return (
     <div className={styles.study_survey}>
-      <Header title="Sorry to see you go" />
+      <div className={styles.study_survey__nav_bar}>
+        <NavBar>
+          <IconButton label="iAgree" flat>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            </svg>
+          </IconButton>
+          <IconButton label="My profile" flat>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+          </IconButton>
+        </NavBar>
+      </div>
+      <div className={styles.study_survey__header}>
+        <Header title="Sorry to see you go" />
+      </div>
+      <div className={styles['study_survey__header--noBackground']}>
+        <Header title="Sorry to see you go" noBackground />
+      </div>
       <div className={styles.study_survey__body}>
-        <div className={styles.study_survey__form}>
-          <div className={styles.study_survey__question}>{question}</div>
-          {radioGroup}
-          <div
-            className={
-              ifOtherSelectedState
-                ? styles.study_survey__comment
-                : [styles.study_survey__comment, styles['study_survey__comment--hidden']].join(' ')
-            }
-          >
-            <Textarea placeholder="Please specify" />
+        <div className={styles.study_survey__content}>
+          <div className={styles.study_survey__form}>
+            <div className={styles.study_survey__question}>{question}</div>
+            {radioGroup}
+            <div
+              className={
+                ifOtherSelectedState
+                  ? styles.study_survey__comment
+                  : [styles.study_survey__comment, styles['study_survey__comment--hidden']].join(' ')
+              }
+            >
+              <Textarea placeholder="Please specify" />
+            </div>
+          </div>
+          <div className={styles.study_survey__button_container}>
+            <Button label="Submit" />
           </div>
         </div>
       </div>
-      <Footer alignContentEvenly={false} sticky={sticky}>
-        <Button label="Submit" />
-      </Footer>
+      <div className={styles.study_survey__footer}>
+        <Footer alignContentEvenly={false} sticky={sticky}>
+          <Button label="Submit" />
+        </Footer>
+      </div>
     </div>
   );
 };
