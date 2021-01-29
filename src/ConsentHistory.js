e08360947a0d988c8fb760f83a2e976e170f748a
Zhaoxian Hu <huzhx@uci.edu>
2021-01-28 21:17:32 -0800

    update ConsentHistory style to be responsive

--- src/ConsentHistory.js
+++ src/ConsentHistory.js
@@ -6,6 +6,7 @@ import Header from './Header';
 import Footer from './Footer';
 import { useScrollPosition } from './UseScrollPosition';
 import IconButton from './IconButton';
+import NavBar from './NavBar';
 
 const ConsentHistory = () => {
   const [sticky, setSticky] = useState(false);
@@ -63,40 +64,79 @@ const ConsentHistory = () => {
 
   return (
     <div className={styles.consent_history}>
-      <Header title="Consent History" />
+      <div className={styles.consent_history__nav_bar}>
+        <NavBar>
+          <IconButton label="iAgree" flat>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            </svg>
+          </IconButton>
+          <IconButton label="My profile" flat>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+          </IconButton>
+        </NavBar>
+      </div>
+      <div className={styles.consent_history__header}>
+        <Header title="Consent History" />
+      </div>
+      <div className={styles['consent_history__header--noBackground']}>
+        <Header title="Consent History" noBackground />
+      </div>
       <div className={styles.consent_history__body}>
         <div className={styles.consent_history__content}>{studies}</div>
       </div>
-      <Footer alignContentEvenly={true} sticky={sticky}>
-        <IconButton label="Home">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-            <polyline points="9 22 9 12 15 12 15 22"></polyline>
-          </svg>
-        </IconButton>
-        <IconButton label="My profile">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-            <circle cx="12" cy="7" r="4"></circle>
-          </svg>
-        </IconButton>
-      </Footer>
+      <div className={styles.consent_history__footer}>
+        <Footer alignContentEvenly={true} sticky={sticky}>
+          <IconButton label="Home">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            </svg>
+          </IconButton>
+          <IconButton label="My profile">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+          </IconButton>
+        </Footer>
+      </div>
     </div>
   );
 };
