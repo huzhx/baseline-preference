17c6b9d118bc8d4e54456a75b302d1df2455abc9
Zhaoxian Hu <huzhx@uci.edu>
2021-01-28 21:03:28 -0800

    update StudyInfo style to be responsive

--- src/StudyInfo.js
+++ src/StudyInfo.js
@@ -6,6 +6,8 @@ import Header from './Header';
 import Button from './Button';
 import { useScrollPosition } from './UseScrollPosition';
 import Footer from './Footer';
+import IconButton from './IconButton';
+import NavBar from './NavBar';
 
 const StudyInfo = () => {
   const [sticky, setSticky] = useState(false);
@@ -26,7 +28,44 @@ const StudyInfo = () => {
   };
   return (
     <div className={styles.study_info}>
-      <Header title="Study Information" />
+      <div className={styles.study_info__nav_bar}>
+        <NavBar>
+          <IconButton label="iAgree" flat>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            </svg>
+          </IconButton>
+          <IconButton label="My profile" flat>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+          </IconButton>
+        </NavBar>
+      </div>
+      <div className={styles.study_info__header}>
+        <Header title="Study Information" />
+      </div>
+      <div className={styles['study_info__header--noBackground']}>
+        <Header title="Study Information" noBackground />
+      </div>
       <div className={styles.study_info__body}>
         <div className={styles.study_info__content}>
           <Study
@@ -36,11 +75,16 @@ const StudyInfo = () => {
             researchers={studyInfo.researchers}
             dateRequested={studyInfo.dateRequested}
           />
+          <div className={styles.study_info__button_container}>
+            <Button label="Next" />
+          </div>
         </div>
       </div>
-      <Footer alignContentEvenly={false} sticky={sticky}>
-        <Button label="Next" />
-      </Footer>
+      <div className={styles.study_info__footer}>
+        <Footer alignContentEvenly={false} sticky={sticky}>
+          <Button label="Next" />
+        </Footer>
+      </div>
     </div>
   );
 };
