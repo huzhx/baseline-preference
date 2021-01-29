9fd4186e6c01ac20a874e1bddf74b0171d614be8
Zhaoxian Hu <huzhx@uci.edu>
2021-01-28 23:07:02 -0800

    update StudyDataSharingForm to take more parameters to show three columns

--- src/StudyDataSharingForm.js
+++ src/StudyDataSharingForm.js
@@ -3,16 +3,38 @@ import React from 'react';
 import styles from './StudyDataSharingForm.module.css';
 import DataSharingElement from './DataSharingElement';
 
-const StudyDataSharingForm = ({ dataElements, requiredElements }) => {
+const StudyDataSharingForm = ({
+  dataElements,
+  requiredElements,
+  usedElements,
+  dataElementsStateMap,
+  dataElementsHandleChangeMap,
+}) => {
   const content = [];
   for (let element of dataElements) {
-    content.push(<DataSharingElement key={element} dataElement={element} required={requiredElements.has(element)} />);
+    content.push(
+      <DataSharingElement
+        key={element}
+        dataElement={element}
+        required={requiredElements.has(element)}
+        checked={dataElementsStateMap && dataElementsStateMap.get(element)}
+        used={usedElements && usedElements.has(element)}
+        onChange={dataElementsHandleChangeMap && dataElementsHandleChangeMap.get(element)}
+      />
+    );
   }
   return (
     <div className={styles.study_data_sharing_form}>
-      <div className={styles.study_data_sharing_form__label_container}>
+      <div
+        className={
+          usedElements
+            ? [styles.study_data_sharing_form__label_container, styles.three_columns].join(' ')
+            : styles.study_data_sharing_form__label_container
+        }
+      >
         <span className={[styles.study_data_sharing_form__label, styles.align_left].join(' ')}>Data Requested</span>
         <span className={styles.study_data_sharing_form__label}>Consent to Share</span>
+        {usedElements && <span className={styles.study_data_sharing_form__label}>Data Used</span>}
       </div>
       {content}
       <div className={styles.study_data_sharing_form__label_container}>
