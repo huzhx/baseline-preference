9ffe753f468d957461b94ba215aab24dff2276b3
Zhaoxian Hu <huzhx@uci.edu>
2021-01-29 00:39:50 -0800

    update Textarea and make it autosize

--- src/Textarea.js
+++ src/Textarea.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import styles from './Textarea.module.css';
 
-const Textarea = ({ label, placeholder, handleOnChange, curValue, rows }) => {
+const Textarea = ({ label, placeholder, onChange, value, rows }) => {
+  const textarea = useRef(null);
+
+  useEffect(() => {
+    textarea.current.focus();
+    textarea.current.rows = textarea.current.scrollHeight / 26;
+  }, [value]);
+
   return (
     <div className={styles.textarea}>
       <label htmlFor="textarea">{label}</label>
-      <textarea className={styles.input} id="textarea" placeholder={placeholder} onChange={handleOnChange} rows={rows}>
-        {curValue}
-      </textarea>
+      <textarea
+        ref={textarea}
+        className={styles.input}
+        id="textarea"
+        placeholder={placeholder}
+        onChange={onChange}
+        rows={rows}
+        value={value}
+      />
     </div>
   );
 };
