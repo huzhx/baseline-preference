265e85d3fe58dbed598ec7f21ef776009b32ddb9
Zhaoxian Hu <huzhx@uci.edu>
2021-01-31 23:49:02 -0800

    update IconButton to take link

--- src/IconButton.js
+++ src/IconButton.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './IconButton.module.css';
 
-const IconButton = ({ label, children, isActive, handleOnClick, flat }) => {
+const IconButton = ({ label, children, isActive, handleOnClick, flat, link = '/' }) => {
   return (
-    <div
-      className={
-        isActive
-          ? [styles.icon_button, styles['icon_button--active'], styles.ripple].join(' ')
-          : [styles.icon_button, styles.ripple].join(' ')
-      }
-      onClick={handleOnClick}
-    >
-      <div className={flat ? [styles.icon_button__layout, styles.flat].join(' ') : styles.icon_button__layout}>
-        <span className={styles.icon_button__svg}>{children}</span>
-        <div className={[styles.icon_button__label, styles.no_select].join(' ')}>{label}</div>
+    <Link className={styles.link} to={link}>
+      <div
+        className={
+          isActive
+            ? [styles.icon_button, styles['icon_button--active'], styles.ripple].join(' ')
+            : [styles.icon_button, styles.ripple].join(' ')
+        }
+        onClick={handleOnClick}
+      >
+        <div className={flat ? [styles.icon_button__layout, styles.flat].join(' ') : styles.icon_button__layout}>
+          <span className={styles.icon_button__svg}>{children}</span>
+          <div className={[styles.icon_button__label, styles.no_select].join(' ')}>{label}</div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
