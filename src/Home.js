6c7998ecfda9b4bc3df9c5147ab72df24c24687c
Zhaoxian Hu <huzhx@uci.edu>
2021-01-31 23:49:46 -0800

    update Home and add the home link

--- src/Home.js
+++ src/Home.js
@@ -64,7 +64,7 @@ const Home = () => {
       </div>
       <div className={styles.home__footer}>
         <Footer alignContentEvenly={true} sticky={sticky}>
-          <IconButton label="Home">
+          <IconButton label="Home" link="/">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
