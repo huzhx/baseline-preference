43dd5250da00aad81daa17cc4755c599bddf17f7
Zhaoxian Hu <huzhx@uci.edu>
2021-01-31 20:22:49 -0800

    update the name of the css class

--- src/PortalPicker.js
+++ src/PortalPicker.js
@@ -10,7 +10,7 @@ const PortalPicker = () => {
         <div className={[styles.portal_picker__header__secondary, styles.secondary_color].join(' ')}>Welcome to</div>
         <div className={styles.portal_picker__header__main}>iAGREE</div>
       </div>
-      <div className={styles['custom-shape-divider-bottom-1612077601']}>
+      <div className={styles['portal_picker__background']}>
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
           <path
             d="M649.97 0L599.91 54.12 550.03 0 0 0 0 120 1200 120 1200 0 649.97 0z"
