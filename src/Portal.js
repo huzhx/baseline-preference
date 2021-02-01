5a2767af7c56db7be286a0c7885086dce7e80db6
Zhaoxian Hu <huzhx@uci.edu>
2021-01-31 20:56:27 -0800

    update the svg selector

--- src/Portal.js
+++ src/Portal.js
@@ -10,7 +10,7 @@ const Portal = ({ portalType }) => {
         <div className={[styles.portal__header__secondary, styles.secondary_color].join(' ')}>Welcome to</div>
         <div className={styles.portal__header__main}>iAGREE</div>
       </div>
-      <div className={styles['portal__background']}>
+      <div className={styles.portal__background}>
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
           <path
             d="M649.97 0L599.91 54.12 550.03 0 0 0 0 120 1200 120 1200 0 649.97 0z"
