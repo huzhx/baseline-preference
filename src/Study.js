3892b05147b8fee99e0594a8622878857ccaf61b
Zhaoxian Hu <huzhx@uci.edu>
2021-02-01 00:14:29 -0800

    update Study component to take link

--- src/Study.js
+++ src/Study.js
@@ -1,43 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './Study.module.css';
 
-const Study = ({ institution, title, status, purpose, researchers, dateRequested }) => {
+const Study = ({ institution, title, status, purpose, researchers, dateRequested, link = '/' }) => {
   return (
-    <div className={styles.study}>
-      <p className={styles.study__content}>
-        <span className={styles['study__content--highlighted']}>Institution:</span>
-        <span>{institution}</span>
-      </p>
-      <p className={styles.study__content}>
-        <span className={styles['study__content--highlighted']}>Study Title:</span>
-        <span>{title}</span>
-      </p>
-      {status && (
+    <Link className={styles.link} to={link}>
+      <div className={[styles.study, styles.ripple].join(' ')}>
         <p className={styles.study__content}>
-          <span className={styles['study__content--highlighted']}>Status:</span>
-          <span>{status}</span>
+          <span className={styles['study__content--highlighted']}>Institution:</span>
+          <span>{institution}</span>
         </p>
-      )}
-      {purpose && (
         <p className={styles.study__content}>
-          <span className={styles['study__content--highlighted']}>Purpose:</span>
-          <span>{purpose}</span>
+          <span className={styles['study__content--highlighted']}>Study Title:</span>
+          <span>{title}</span>
         </p>
-      )}
-      {researchers && (
-        <p className={styles.study__content}>
-          <span className={styles['study__content--highlighted']}>Researchers:</span>
-          <span>{researchers}</span>
-        </p>
-      )}
-      {dateRequested && (
-        <p className={styles.study__content}>
-          <span className={styles['study__content--highlighted']}>Date Requested:</span>
-          <span>{dateRequested}</span>
-        </p>
-      )}
-    </div>
+        {status && (
+          <p className={styles.study__content}>
+            <span className={styles['study__content--highlighted']}>Status:</span>
+            <span>{status}</span>
+          </p>
+        )}
+        {purpose && (
+          <p className={styles.study__content}>
+            <span className={styles['study__content--highlighted']}>Purpose:</span>
+            <span>{purpose}</span>
+          </p>
+        )}
+        {researchers && (
+          <p className={styles.study__content}>
+            <span className={styles['study__content--highlighted']}>Researchers:</span>
+            <span>{researchers}</span>
+          </p>
+        )}
+        {dateRequested && (
+          <p className={styles.study__content}>
+            <span className={styles['study__content--highlighted']}>Date Requested:</span>
+            <span>{dateRequested}</span>
+          </p>
+        )}
+      </div>
+    </Link>
   );
 };
 
