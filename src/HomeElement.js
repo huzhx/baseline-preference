e30701b96c0d80387c851040bf1dba5adb0649aa
Zhaoxian Hu <huzhx@uci.edu>
2021-01-31 23:37:48 -0800

    update HomeElement to handle the link

--- src/HomeElement.js
+++ src/HomeElement.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './HomeElement.module.css';
 
-const HomeElement = ({ label, unreadNum = 0 }) => {
+const HomeElement = ({ label, unreadNum = 0, link }) => {
   return (
-    <div className={[styles.home_element, styles.ripple].join(' ')}>
-      {unreadNum > 0 ? (
-        <div>
-          <div className={styles.home_element__badge}>{unreadNum}</div>
-        </div>
-      ) : (
-        <div></div>
-      )}
-      <div className={[styles.home_element__label, styles.no_select].join(' ')}>{label}</div>
-    </div>
+    <Link className={styles.link} to={link}>
+      <div className={[styles.home_element, styles.ripple].join(' ')}>
+        {unreadNum > 0 ? (
+          <div>
+            <div className={styles.home_element__badge}>{unreadNum}</div>
+          </div>
+        ) : (
+          <div></div>
+        )}
+        <div className={[styles.home_element__label, styles.no_select].join(' ')}>{label}</div>
+      </div>
+    </Link>
   );
 };
 
