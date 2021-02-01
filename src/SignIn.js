4a84b97823c6e33de84bdd97180b5cb061e0ba65
Zhaoxian Hu <huzhx@uci.edu>
2021-01-31 23:04:32 -0800

    update SignIn component to be responsive

--- src/SignIn.js
+++ src/SignIn.js
@@ -3,11 +3,50 @@ import React from 'react';
 import styles from './SignIn.module.css';
 import Header from './Header.js';
 import IconCircleButton from './IconCircleButton';
+import IconButton from './IconButton';
+import NavBar from './NavBar';
 
 const SignIn = () => {
   return (
     <div className={styles.sign_in}>
-      <Header title="Sign in with Social Networks" />
+      <div className={styles.sign_in__nav_bar}>
+        <NavBar>
+          <IconButton label="iAgree" flat>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            </svg>
+          </IconButton>
+          <IconButton label="My profile" flat>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+          </IconButton>
+        </NavBar>
+      </div>
+      <div className={styles.sign_in__header}>
+        <Header title="Sign in with social networks" />
+      </div>
+      <div className={styles['sign_in__header--noBackground']}>
+        <Header title="Sign in with social networks" noBackground />
+      </div>
       <div className={styles.sign_in__body}>
         <div className={styles.sign_in__content}>
           <div className={styles.sign_in__content__button_container}>
