ae95666caffdbe954847b70e0ca290c2c0e7f743
Zhaoxian Hu <huzhx@uci.edu>
2021-02-02 07:55:02 -0800

    update SignIn to provide the handleClick func to IconCircleButton

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import styles from './SignIn.module.css';
 import Header from './Header.js';
@@ -7,6 +8,9 @@ import IconButton from './IconButton';
 import NavBar from './NavBar';
 
 const SignIn = () => {
+  const history = useHistory();
+  const handleClick = () => history.push('/sign-in/verify-options');
+
   return (
     <div className={styles.sign_in}>
       <div className={styles.sign_in__nav_bar}>
@@ -42,7 +46,7 @@ const SignIn = () => {
         </NavBar>
       </div>
       <div className={styles.sign_in__header}>
-        <Header title="Sign in with social networks" />
+        <Header title="Sign in with social networks" hasGoBack />
       </div>
       <div className={styles['sign_in__header--noBackground']}>
         <Header title="Sign in with social networks" noBackground />
@@ -50,7 +54,7 @@ const SignIn = () => {
       <div className={styles.sign_in__body}>
         <div className={styles.sign_in__content}>
           <div className={styles.sign_in__content__button_container}>
-            <IconCircleButton>
+            <IconCircleButton handleClick={handleClick}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="32"
@@ -65,7 +69,7 @@ const SignIn = () => {
                 <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
               </svg>
             </IconCircleButton>
-            <IconCircleButton>
+            <IconCircleButton handleClick={handleClick}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="32"
@@ -81,7 +85,7 @@ const SignIn = () => {
                 <path d="M11.99 13.9v-3.72h9.36c.14.63.25 1.22.25 2.05c0 5.71-3.83 9.77-9.6 9.77c-5.52 0-10-4.48-10-10S6.48 2 12 2c2.7 0 4.96.99 6.69 2.61l-2.84 2.76c-.72-.68-1.98-1.48-3.85-1.48c-3.31 0-6.01 2.75-6.01 6.12s2.7 6.12 6.01 6.12c3.83 0 5.24-2.65 5.5-4.22h-5.51v-.01z" />
               </svg>
             </IconCircleButton>
-            <IconCircleButton>
+            <IconCircleButton handleClick={handleClick}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="32"
