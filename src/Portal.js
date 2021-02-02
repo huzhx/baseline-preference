c774bbb8b3b19958b540b3172c94edb6ab8b7af1
Zhaoxian Hu <huzhx@uci.edu>
2021-02-02 00:02:05 -0800

    update Portal component to handle the back button

--- src/Portal.js
+++ src/Portal.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 
 import styles from './Portal.module.css';
 import AuthButton from './AuthButton';
 
-const Portal = ({ portalType }) => {
+const Portal = () => {
+  const { portalType } = useParams();
+  const history = useHistory();
+  const goBack = () => history.goBack();
+  const goForward = () => history.push('/sign-in');
+
   return (
     <div className={styles.portal}>
       <div className={styles.portal__header}>
+        <div
+          className={[styles.portal__header__back_button_container, styles['portal__header__back_button_container--show']].join(
+            ' '
+          )}
+        >
+          <div className={[styles.portal__header__back_button, styles.ripple].join(' ')} onClick={goBack}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="19" y1="12" x2="5" y2="12"></line>
+              <polyline points="12 19 5 12 12 5"></polyline>
+            </svg>
+          </div>
+        </div>
         <div className={[styles.portal__header__secondary, styles.secondary_color].join(' ')}>Welcome to</div>
         <div className={styles.portal__header__main}>iAGREE</div>
       </div>
@@ -21,7 +47,7 @@ const Portal = ({ portalType }) => {
       <div className={styles.portal__body}>
         <div className={styles.portal__content}>
           <div className={styles.portal__content__label}>{portalType} portal</div>
-          <AuthButton label="Sign in" />
+          <AuthButton label="Sign in" handleClick={goForward} />
           <AuthButton stroke label="Sign up" />
         </div>
       </div>
