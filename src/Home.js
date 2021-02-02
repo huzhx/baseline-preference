088b1f3d4bea094add38cab5edf4f121f8a3890e
Zhaoxian Hu <huzhx@uci.edu>
2021-02-02 08:19:40 -0800

    update the link

--- src/Home.js
+++ src/Home.js
@@ -59,7 +59,7 @@ const Home = () => {
         <div className={styles.home__content}>
           <HomeElement label="New Data Requests" unreadNum="2" link="/data-requests" />
           <HomeElement label="Consent History" link="/consent-history" />
-          <HomeElement label="Default Data Sharing Preferences" link="/baseline-preference" />
+          <HomeElement label="Default Data Sharing Preferences" link="/baseline-preference/demographic" />
         </div>
       </div>
       <div className={styles.home__footer}>
