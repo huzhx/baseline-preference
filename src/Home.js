cdc74c0b83e13fa28668e0c94aee4299de1df718
Zhaoxian Hu <huzhx@uci.edu>
2021-02-01 21:31:36 -0800

    set home button active

--- src/Home.js
+++ src/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
     <div className={styles.home}>
       <div className={styles.home__nav_bar}>
         <NavBar>
-          <IconButton label="iAgree" flat>
+          <IconButton label="iAgree" flat link="/" isActive>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -64,7 +64,7 @@ const Home = () => {
       </div>
       <div className={styles.home__footer}>
         <Footer alignContentEvenly={true} sticky={sticky}>
-          <IconButton label="Home" link="/">
+          <IconButton label="Home" link="/" isActive>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
