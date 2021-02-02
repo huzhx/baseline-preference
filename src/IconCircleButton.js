413a36c807b71046bc3be268c6e6b25a0be18014
Zhaoxian Hu <huzhx@uci.edu>
2021-02-02 07:53:57 -0800

    update IconCircleButton to haandle click

--- src/IconCircleButton.js
+++ src/IconCircleButton.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import styles from './IconCircleButton.module.css';
 
-const IconCircleButton = ({ children }) => {
-  return <div className={[styles.icon_circle_button, styles.ripple].join(' ')}>{children}</div>;
+const IconCircleButton = ({ children, handleClick }) => {
+  return (
+    <div className={[styles.icon_circle_button, styles.ripple].join(' ')} onClick={handleClick}>
+      {children}
+    </div>
+  );
 };
 
 export default IconCircleButton;
