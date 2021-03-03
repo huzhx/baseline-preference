d30064a64de58f470af1e6a4a6be16534ed6c401
Zhaoxian Hu <huzhx@uci.edu>
2021-03-02 16:29:59 -0800

    add StudyRequest component that contains a Study and a button handler

---
+++ src/StudyRequest.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './StudyRequest.module.css';
+import Study from './Study';
+import Button from './Button';
+
+const StudyRequest = ({ studyInfo, buttonLabel, buttonHandler }) => {
+  return (
+    <div className={styles.study_request}>
+      <Study key={studyInfo.title} institution={studyInfo.institution} title={studyInfo.title} />
+      <div className={styles.study_request__button_container}>
+        <Button label={buttonLabel} handleClick={buttonHandler} />
+      </div>
+    </div>
+  );
+};
+
+export default StudyRequest;
