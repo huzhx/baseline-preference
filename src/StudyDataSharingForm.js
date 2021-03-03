c43648780f6fc79fd9148cbffdd6784686846003
Zhaoxian Hu <huzhx@uci.edu>
2021-03-02 21:59:39 -0800

    remove the dataElements and use requiredElements to set up the DataSharingElement and remove the Required * label

--- src/StudyDataSharingForm.js
+++ src/StudyDataSharingForm.js
@@ -4,19 +4,17 @@ import styles from './StudyDataSharingForm.module.css';
 import DataSharingElement from './DataSharingElement';
 
 const StudyDataSharingForm = ({
-  dataElements,
   requiredElements,
   usedElements,
   dataElementsStateMap,
   dataElementsHandleChangeMap,
 }) => {
   const content = [];
-  for (let element of dataElements) {
+  for (let element of requiredElements) {
     content.push(
       <DataSharingElement
         key={element}
         dataElement={element}
-        required={requiredElements.has(element)}
         checked={dataElementsStateMap && dataElementsStateMap.get(element)}
         used={usedElements && usedElements.has(element)}
         onChange={dataElementsHandleChangeMap && dataElementsHandleChangeMap.get(element)}
@@ -37,9 +35,6 @@ const StudyDataSharingForm = ({
         {usedElements && <span className={styles.study_data_sharing_form__label}>Data Used</span>}
       </div>
       {content}
-      <div className={styles.study_data_sharing_form__label_container}>
-        <span className={[styles.study_data_sharing_form__label, styles.align_left].join(' ')}>*Required</span>
-      </div>
     </div>
   );
 };
