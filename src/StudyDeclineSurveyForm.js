e1e2a99c3090646b22b0b61b78de674f1142cc2a
Zhaoxian Hu <huzhx@uci.edu>
2021-03-08 19:36:30 -0800

    let StudyDeclineSurveyForm to handle the option and question; highlight the data element in the question

--- src/StudyDeclineSurveyForm.js
+++ src/StudyDeclineSurveyForm.js
@@ -5,14 +5,19 @@ import RadioWithOption from './RadioWithOption';
 import Textarea from './Textarea';
 
 const StudyDeclineSurveyForm = ({
-  question,
-  options,
+  declinedElement,
   declineState,
   handleDeclineChange,
-  ifOtherSelectedState,
-  handleCommentChange,
   commentState,
+  handleCommentChange,
 }) => {
+  const options = [
+    'a. I am concerned about the security of the information I provided',
+    'b. I am not interested in this topic',
+    'c. Other',
+    'd. Decline to answer',
+  ];
+
   const radioGroup = [];
   for (let option of options) {
     radioGroup.push(
@@ -26,18 +31,14 @@ const StudyDeclineSurveyForm = ({
   }
   return (
     <div className={styles.study_decline_survey_form}>
-      <div className={styles.study_decline_survey_form__question}>{question}</div>
+      <div className={styles.study_decline_survey_form__question}>
+        We noticed that you declined to share
+        <span className={styles['study_decline_survey_form__question--highlighted']}> {declinedElement}</span> data with
+        us for this study. Could you please tell us why?
+      </div>
       {radioGroup}
-      <div
-        className={
-          ifOtherSelectedState
-            ? styles.study_decline_survey_form__comment
-            : [styles.study_decline_survey_form__comment, styles['study_decline_survey_form__comment--hidden']].join(
-                ' '
-              )
-        }
-      >
-        <Textarea placeholder="Please specify" onChange={handleCommentChange} value={commentState} />
+      <div className={styles.study_decline_survey_form__comment}>
+        <Textarea placeholder="Any additional information" onChange={handleCommentChange} value={commentState} />
       </div>
     </div>
   );
