d3a525bb4ca9b3cf1e292689ab53b0b90998a580
Zhaoxian Hu <huzhx@uci.edu>
2021-03-23 18:27:58 -0700

    update form to handle alternate views

--- src/BaselinePreferenceForm.js
+++ src/BaselinePreferenceForm.js
@@ -13,15 +13,32 @@ import IconButton from './IconButton';
 const BaselinePreferenceForm = ({ cb }) => {
   const { pathname } = useLocation();
 
-  const { dataElement } = useParams();
-  const nextPath = cb(dataElement);
+  const { index, dataElement, org } = useParams();
+  console.log({ index, dataElement, org, cb });
+  let nextPath = '';
+  if (dataElement) nextPath = cb(`v1-${dataElement}`);
+  if (org) nextPath = cb(`v2-${org}`);
+  console.log({ nextPath });
   const history = useHistory();
   const goBack = () => history.goBack();
   const handleClick = () => {
     history.push(nextPath);
   };
 
+  let title = '';
+  let addon = '';
+  let highlights = '';
+  if (dataElement) {
+    title = `${index}. Would you share your`;
+    addon = 'information with';
+    highlights = dataElement.split('-').join(' ');
+  } else {
+    title = `${index}. Would you share the information below with`;
+    highlights = org.split('-').join(' ');
+  }
+
   const [sticky, setSticky] = useState(false);
+  // for v1
   const [doctorOfficeState, setDoctorOfficeState] = useState('');
   const [hospitalState, setHospitalState] = useState('');
   const [insuranceState, setInsuranceState] = useState('');
@@ -29,7 +46,15 @@ const BaselinePreferenceForm = ({ cb }) => {
   const [governmentHAState, setGovernmentHAState] = useState('');
   const [biotechnologyState, setBiotechnologyState] = useState('');
   const [universityState, setUniversityState] = useState('');
-
+  // for v2
+  const [demographicState, setDemographicState] = useState('');
+  const [generalClinicalState, setGeneralClinicalState] = useState('');
+  const [biospecimenState, setBiospecimenState] = useState('');
+  const [geneticState, setGeneticState] = useState('');
+  const [mentalHealthState, setMentalHealthState] = useState('');
+  const [sexAndReproductiveState, setSexAndReproductiveState] = useState('');
+  const [familyHistoryState, setFamilyHistoryState] = useState('');
+  // for v1
   const handleDoctorOfficeChange = (event) => {
     setDoctorOfficeState(event.target.value);
   };
@@ -51,6 +76,28 @@ const BaselinePreferenceForm = ({ cb }) => {
   const handleUniversityChange = (event) => {
     setUniversityState(event.target.value);
   };
+  // for v2
+  const handleDemographicChange = (event) => {
+    setDemographicState(event.target.value);
+  };
+  const handleGeneralClinicalChange = (event) => {
+    setGeneralClinicalState(event.target.value);
+  };
+  const handleBiospecimenChange = (event) => {
+    setBiospecimenState(event.target.value);
+  };
+  const handleGeneticChange = (event) => {
+    setGeneticState(event.target.value);
+  };
+  const handleMentalHealthChange = (event) => {
+    setMentalHealthState(event.target.value);
+  };
+  const handleSexAndReproductiveChange = (event) => {
+    setSexAndReproductiveState(event.target.value);
+  };
+  const handleFamilyHistoryChange = (event) => {
+    setFamilyHistoryState(event.target.value);
+  };
 
   useScrollPosition(
     ({ prevPos, currPos }) => {
@@ -62,6 +109,7 @@ const BaselinePreferenceForm = ({ cb }) => {
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    // for v1
     setDoctorOfficeState('');
     setHospitalState('');
     setInsuranceState('');
@@ -69,10 +117,19 @@ const BaselinePreferenceForm = ({ cb }) => {
     setGovernmentHAState('');
     setBiotechnologyState('');
     setUniversityState('');
+    // for v2
+    setDemographicState('');
+    setGeneralClinicalState('');
+    setBiospecimenState('');
+    setGeneticState('');
+    setMentalHealthState('');
+    setSexAndReproductiveState('');
+    setFamilyHistoryState('');
   }, [pathname]);
 
   useEffect(() => {
     console.log({
+      // for v1
       doctorOfficeState,
       hospitalState,
       insuranceState,
@@ -80,8 +137,17 @@ const BaselinePreferenceForm = ({ cb }) => {
       governmentHAState,
       biotechnologyState,
       universityState,
+      // for v2
+      demographicState,
+      generalClinicalState,
+      biospecimenState,
+      geneticState,
+      mentalHealthState,
+      sexAndReproductiveState,
+      familyHistoryState,
     });
   }, [
+    // for v1
     doctorOfficeState,
     hospitalState,
     insuranceState,
@@ -89,9 +155,18 @@ const BaselinePreferenceForm = ({ cb }) => {
     governmentHAState,
     biotechnologyState,
     universityState,
+    // for v2
+    demographicState,
+    generalClinicalState,
+    biospecimenState,
+    geneticState,
+    mentalHealthState,
+    sexAndReproductiveState,
+    familyHistoryState,
   ]);
 
-  const labels = [
+  // for v1
+  const orgLabels = [
     'A doctor’s office',
     'A hospital',
     'An insurance company',
@@ -100,41 +175,85 @@ const BaselinePreferenceForm = ({ cb }) => {
     'A biotechnology company',
     'A college or university',
   ];
+  // for v2
+  const dataLabels = [
+    'Demographic information',
+    'General clinical information',
+    'Biospecimen information',
+    'Genetic information',
+    'Mental health information',
+    'Sexual and reproductive health information',
+    'Family history information',
+  ];
   const labelStateMap = new Map();
   const labelHandleChangeMap = new Map();
 
-  labelStateMap.set(labels[0], doctorOfficeState);
-  labelStateMap.set(labels[1], hospitalState);
-  labelStateMap.set(labels[2], insuranceState);
-  labelStateMap.set(labels[3], stateLocalHAState);
-  labelStateMap.set(labels[4], governmentHAState);
-  labelStateMap.set(labels[5], biotechnologyState);
-  labelStateMap.set(labels[6], universityState);
+  // for v1
+  labelStateMap.set(orgLabels[0], doctorOfficeState);
+  labelStateMap.set(orgLabels[1], hospitalState);
+  labelStateMap.set(orgLabels[2], insuranceState);
+  labelStateMap.set(orgLabels[3], stateLocalHAState);
+  labelStateMap.set(orgLabels[4], governmentHAState);
+  labelStateMap.set(orgLabels[5], biotechnologyState);
+  labelStateMap.set(orgLabels[6], universityState);
 
-  labelHandleChangeMap.set(labels[0], handleDoctorOfficeChange);
-  labelHandleChangeMap.set(labels[1], handleHospitalChange);
-  labelHandleChangeMap.set(labels[2], handleInsuranceChange);
-  labelHandleChangeMap.set(labels[3], handleStateLocalHAChange);
-  labelHandleChangeMap.set(labels[4], handleGovernmentHAChange);
-  labelHandleChangeMap.set(labels[5], handleBiotechnologyChange);
-  labelHandleChangeMap.set(labels[6], handleUniversityChange);
+  labelHandleChangeMap.set(orgLabels[0], handleDoctorOfficeChange);
+  labelHandleChangeMap.set(orgLabels[1], handleHospitalChange);
+  labelHandleChangeMap.set(orgLabels[2], handleInsuranceChange);
+  labelHandleChangeMap.set(orgLabels[3], handleStateLocalHAChange);
+  labelHandleChangeMap.set(orgLabels[4], handleGovernmentHAChange);
+  labelHandleChangeMap.set(orgLabels[5], handleBiotechnologyChange);
+  labelHandleChangeMap.set(orgLabels[6], handleUniversityChange);
+
+  // for v2
+  labelStateMap.set(dataLabels[0], demographicState);
+  labelStateMap.set(dataLabels[1], generalClinicalState);
+  labelStateMap.set(dataLabels[2], biospecimenState);
+  labelStateMap.set(dataLabels[3], geneticState);
+  labelStateMap.set(dataLabels[4], mentalHealthState);
+  labelStateMap.set(dataLabels[5], sexAndReproductiveState);
+  labelStateMap.set(dataLabels[6], familyHistoryState);
+
+  labelHandleChangeMap.set(dataLabels[0], handleDemographicChange);
+  labelHandleChangeMap.set(dataLabels[1], handleGeneralClinicalChange);
+  labelHandleChangeMap.set(dataLabels[2], handleBiospecimenChange);
+  labelHandleChangeMap.set(dataLabels[3], handleGeneticChange);
+  labelHandleChangeMap.set(dataLabels[4], handleMentalHealthChange);
+  labelHandleChangeMap.set(dataLabels[5], handleSexAndReproductiveChange);
+  labelHandleChangeMap.set(dataLabels[6], handleFamilyHistoryChange);
 
   const options = ['Yes', 'No'];
 
   const baselinePreferenceFormElements = [];
 
-  for (let label of labels) {
-    const curCheckedValue = labelStateMap.get(label);
-    const handleChange = labelHandleChangeMap.get(label);
-    baselinePreferenceFormElements.push(
-      <BaselinePreferenceFormElement
-        key={label}
-        label={label}
-        options={options}
-        curCheckedValue={curCheckedValue}
-        handleChange={handleChange}
-      />
-    );
+  if (dataElement) {
+    for (let label of orgLabels) {
+      const curCheckedValue = labelStateMap.get(label);
+      const handleChange = labelHandleChangeMap.get(label);
+      baselinePreferenceFormElements.push(
+        <BaselinePreferenceFormElement
+          key={label}
+          label={label}
+          options={options}
+          curCheckedValue={curCheckedValue}
+          handleChange={handleChange}
+        />
+      );
+    }
+  } else {
+    for (let label of dataLabels) {
+      const curCheckedValue = labelStateMap.get(label);
+      const handleChange = labelHandleChangeMap.get(label);
+      baselinePreferenceFormElements.push(
+        <BaselinePreferenceFormElement
+          key={label}
+          label={label}
+          options={options}
+          curCheckedValue={curCheckedValue}
+          handleChange={handleChange}
+        />
+      );
+    }
   }
 
   const buttonLabel = 'next';
@@ -174,20 +293,10 @@ const BaselinePreferenceForm = ({ cb }) => {
         </NavBar>
       </div>
       <div className={styles.baseline_preference_form__header}>
-        <Header
-          title="How comfortable are you with sharing your"
-          highlights={dataElement.split('-').join(' ')}
-          addon="information"
-          hasGoBack
-        />
+        <Header title={title} highlights={highlights} addon={addon} hasGoBack />
       </div>
       <div className={styles['baseline_preference_form__header--noBackground']}>
-        <Header
-          title="How comfortable are you with sharing your"
-          highlights={dataElement.split('-').join(' ')}
-          addon="information"
-          noBackground
-        />
+        <Header title={title} highlights={highlights} addon={addon} noBackground />
       </div>
       <div className={styles.baseline_preference_form__body}>
         <div className={styles.baseline_preference_form__content}>
