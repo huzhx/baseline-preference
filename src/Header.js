6b84d9950c031ba4b1c51f6df0ef5628f29b8340
Zhaoxian Hu <huzhx@uci.edu>
2021-04-27 15:00:44 -0700

    add tooltipData prop to Header

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 
 import styles from './Header.module.css';
 
-const Header = ({ title, highlights, addon, noBackground, hasGoBack }) => {
+const Header = ({ title, highlights, addon, noBackground, hasGoBack, tooltipData }) => {
   const history = useHistory();
   const goBack = () => history.goBack();
 
@@ -33,7 +33,28 @@ const Header = ({ title, highlights, addon, noBackground, hasGoBack }) => {
       </div>
       <div className={[styles.header__main, styles.no_select].join(' ')}>
         {title}
-        {highlights && <span className={styles['header--highlighted']}> {highlights} </span>}
+        {
+          <span className={styles.header__inline_block}>
+            {highlights && <span className={styles['header--highlighted']}> {` ${highlights}`} </span>}
+            {tooltipData && (
+              <span className={[styles['header--highlighted'], styles.header__svg].join(' ')}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <circle cx="12" cy="12" r="10"></circle>
+                  <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
+                  <line x1="12" y1="17" x2="12.01" y2="17"></line>
+                </svg>
+              </span>
+            )}
+          </span>
+        }
         {addon}
       </div>
     </div>
