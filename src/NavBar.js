11be3dca16558962f8fc14915e2ab03b4ef28a47
Zhaoxian Hu <huzhx@uci.edu>
2021-05-03 13:26:25 -0700

    adjust NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 import styles from './NavBar.module.css';
+import IconButton from './IconButton';
+import { MdHome } from 'react-icons/md';
 
 const NavBar = ({ children }) => {
   return (
-    <div className={styles.nav_bar}>
-      <div className={styles.nav_bar__content}>{children}</div>
-    </div>
+    <nav className={styles.nav_bar}>
+      <div className={styles.nav_bar__content}>
+        <div className={styles.nav_bar__home}>
+          <IconButton label="iAgree" flat link="/">
+            <MdHome />
+          </IconButton>
+        </div>
+        <ul className={styles.nav_bar__items}>{children}</ul>
+      </div>
+    </nav>
   );
 };
 
