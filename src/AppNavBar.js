e63c2e39aaa4ff9458c9803ab3e31558f8cde4c9
Zhaoxian Hu <huzhx@uci.edu>
2021-05-03 22:51:13 -0700

    implement Profile component

--- src/AppNavBar.js
+++ src/AppNavBar.js
@@ -1,15 +1,14 @@
 import NavBar from './NavBar';
-import NavItem from './NavItem';
-import { MdAccountCircle } from 'react-icons/md';
 import ThemeSelector from './ThemeSelector';
 import FontSizeSelector from './FontSizeSelector';
+import Profile from './Profile';
 
 const AppNavBar = () => {
   return (
     <NavBar>
       <FontSizeSelector />
       <ThemeSelector />
-      <NavItem icon={<MdAccountCircle />} />
+      <Profile />
     </NavBar>
   );
 };
