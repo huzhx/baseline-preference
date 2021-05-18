0c43273e22f9cae15c8c37dc62f45f0d2d5a1fbc
Zhaoxian Hu <huzhx@uci.edu>
2021-05-18 12:31:50 -0700

    :construction: implement NotificationPreferenceElement

--- src/NotificationPreference.js
+++ src/NotificationPreference.js
@@ -1,5 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import NotificationPreferenceElement from './NotificationPreferenceElement';
+
 const NotificationPreference = () => {
-  return <div testId="notificationPreferenceContainer">Notifcation preference setting</div>;
+  const Form = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 80%;
+    margin: auto;
+  `;
+
+  const labels = [
+    'Please notify me when there are new studies that may benefit from using my data.',
+    'Please notify me when my data are used by studies with which I agreed to share my data.',
+  ];
+
+  const content = [];
+  for (let label of labels) {
+    content.push(<NotificationPreferenceElement key={label} label={label} />);
+  }
+
+  return (
+    <div testId="notificationPreferenceContainer">
+      <Form>{content}</Form>
+    </div>
+  );
 };
 
 export default NotificationPreference;
