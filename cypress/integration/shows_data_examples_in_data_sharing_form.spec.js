69e8bc653b7247c45c90753985532beef6ba7102
Zhaoxian Hu <huzhx@uci.edu>
2021-05-18 01:24:02 -0700

    add test for the data request review form

--- cypress/integration/shows_data_examples_in_data_sharing_form.spec.js
+++ cypress/integration/shows_data_examples_in_data_sharing_form.spec.js
@@ -1,6 +1,19 @@
 describe('shows data examples in the data sharing forms', () => {
-  it('shows data example in the new data request form', () => {
+  it('shows data examples in the new data request form', () => {
     cy.visit(`${Cypress.env('BASE_URL')}/data-requests`);
+    cy.get('[data-testid="studyRequestButtonContainer"]')
+      .first()
+      .find('button')
+      .click()
+      .get('[data-testid="studyInfoButtonContainer"]')
+      .find('button')
+      .contains(/next/i)
+      .click()
+      .get('[data-testid="dataExamples"]');
+  });
+
+  it('shows data examples in the data request review form', () => {
+    cy.visit(`${Cypress.env('BASE_URL')}/consent-history`);
     cy.get('[data-testid="studyRequestButtonContainer"]')
       .first()
       .find('button')
