ed5182d6ee91f814a817017a705be51559c064cc
Zhaoxian Hu <huzhx@uci.edu>
2021-05-18 00:15:41 -0700

    add data examples

---
+++ src/models/dataExamples.js
@@ -0,0 +1,10 @@
+export default dataExamples = {
+  demographic: 'Age, Sex, Race, Ethnicity, Sexual Orientation',
+  'general clinical': '',
+  biospecimen: 'Tissue, Blood, Urine',
+  genetic: '',
+  'mental health': '',
+  'sexual and reproductive health': '',
+  'family history':
+    'Substance abuse related disease or condition, Mental health disease or condition, Sexual or reproductive disease or condition',
+};
